5873699ce73f22f0421559dd9061a757341787fe
Matutozi <sobowalegz2@gmail.com>
2024-09-16 14:26:43 +0100

    FEAT: create the managers file to handle the user management

--- api/users/models.py
+++ api/users/models.py
@@ -1,33 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 import uuid
-
-
-class UserManager(BaseUserManager):
-    
-    def create_user(self, email, username, password=None, **extra_fields):
-        if not email:
-            return ValueError("The Email Field must be set")
-        if not username:
-            return ValueError("Username must be set")
-        
-        email = self.normalize_email(email)
-        user = self.model(email=email, username=username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    
-
-    def create_superuser(self, email, username, password=None, **extra_fields):
-        extra_fields.setdefault('is_admin', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_admin') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(email, username, password, **extra_fields)
+from .managers import UserManager
 
 
 

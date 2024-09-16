612fc1af05c37be49bd0d34249ad2901dac376f2
Matutozi <sobowalegz2@gmail.com>
2024-09-16 15:20:34 +0100

    FEAT: models that manage all notification functionality

--- api/notifications/models.py
+++ api/notifications/models.py
@@ -1,3 +1,26 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+import uuid
+from users.models import User
+
+
+class Notification(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=[('read', 'Read'), ('unread', 'Unread')], default='unread')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+
+    def __str__(self):
+        return f"Notification for {self.user.username}"
+
+
+class NotificationSetting(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    enable = models.BooleanField(default=False)
+    keywords = models.JSONField()  
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Settings for {self.user.username}"

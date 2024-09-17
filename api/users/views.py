dcfdd645926bb62acb59df14be94bb9b4a327bcc
Matutozi <sobowalegz2@gmail.com>
2024-09-17 21:42:22 +0100

    fix: implement stateless simplejwt for login and logout and user views

--- api/users/views.py
+++ api/users/views.py
@@ -3,9 +3,15 @@ from rest_framework.views import APIView
 from .serializers import UserSerializers, CreateUserSerializer
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
 from rest_framework import status
 from .models import User
 import jwt, datetime
+from django.conf import settings
+
+JWT_SECRET = settings.SECRET_KEY
 
 
 class RegisterView(APIView):
@@ -21,8 +27,8 @@ class LoginView(APIView):
         email = request.data.get("email")
         password = request.data.get("password")
 
-        if password is None:
-            raise AuthenticationFailed("Password Required")
+        if not email or not password:
+            raise AuthenticationFailed("Email and Password required")
 
         user = User.objects.filter(email=email).first()
 
@@ -41,12 +47,13 @@ class LoginView(APIView):
 
         payload = {
             "id": str(user.id),
+            "email": user.email,
             "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
             "iat": datetime.datetime.utcnow(),
         }
 
-        token = jwt.encode(payload, "secret", algorithm="HS256")
-        print(token)
+        token = jwt.encode(payload, JWT_SECRET, algorithm="HS256")
+
         response_data = {
             "status_code": status.HTTP_200_OK,
             "jwt": token,
@@ -59,6 +66,20 @@ class LoginView(APIView):
 
 class LogoutView(APIView):
     def post(self, request):
+        auth_header = request.headers.get("Authorization")
+
+        if not auth_header or not auth_header.startswith("Bearer "):
+            return Response({"detail": "No token provided"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        token = auth_header.split(" ")[1]
+
+        try:
+            refresh_token = RefreshToken(token)
+            refresh_token.blacklist()
+        except TokenError:
+            raise AuthenticationFailed("Invalid Token")
+        
+
         response_data = {
             "status_code": status.HTTP_200_OK,
             "message": "Logout Successful.",
@@ -66,36 +87,40 @@ class LogoutView(APIView):
         }
         return Response(response_data)
 
+
 class UserView(APIView):
     def get(self, request):
-        print("Hell")
+        print("USER VIEW")
         auth_head = request.headers.get("Authorization")
 
         #print(auth_head)
 
         if not auth_head or not auth_head.startswith("Bearer "):
-            raise AuthenticationFailed("Unauthenticated")
+            raise AuthenticationFailed("Unauthenticated-  No token provided")
 
         token = auth_head.split(" ")[1]
         print(token)
 
         try:
-            payload = jwt.decode(token, "secret", algorithm="HS256")
+            payload = jwt.decode(token, JWT_SECRET, algorithm="HS256")
         except jwt.ExpiredSignatureError:
-            raise AuthenticationFailed("Unauthenticated")
+            raise AuthenticationFailed("Unauthenticated- Token has expired")
         except jwt.InvalidTokenError:
             raise AuthenticationFailed("Invalid token")
 
         print(payload)
 
-        user = User.objects.get(id=payload["id"])
-        serializer = UserSerializers(user)
+        user_data = {
+            "id": payload["id"],
+            "email": payload["email"],
+
+        }
 
         response = Response(
             {
                 "status_code": status.HTTP_200_OK,
                 "message": "User details fetched successfully.",
-                "data": serializer.data,
+                "data": user_data
             }
         )
 

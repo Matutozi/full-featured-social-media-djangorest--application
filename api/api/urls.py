6bb96065e49c5e3efb28fb2b4f45be2618cf8c6f
Matutozi <sobowalegz2@gmail.com>
2024-09-18 19:47:19 +0100

    chore:setup for jwt

--- api/api/urls.py
+++ api/api/urls.py
@@ -14,12 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenObtainSlidingView,
+    TokenRefreshSlidingView,
+    TokenBlacklistView,
+)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
+    path("admin/", admin.site.urls),
+    path("api/token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
+    path("api/token/", TokenObtainSlidingView.as_view(), name="token_obtain"),
+    path("api/token/refresh/", TokenRefreshSlidingView.as_view(), name="token_refresh"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/users/", include("users.urls")),
-    
 ]

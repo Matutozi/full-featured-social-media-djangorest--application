389eefa2ae901fc2ef13e482ad4fa46bf160d9f0
Matutozi <sobowalegz2@gmail.com>
2024-09-24 00:14:11 +0100

    chore: update the readme for functionality

--- api/posts/admin.py
+++ api/posts/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 from .models import Post, PostComment, PostReaction, Hashtag
 
+
 # Register your models here.
 class PostsAdmin(admin.ModelAdmin):
     list_display = ["user", "updated_at"]
     search_fields = ["user"]
+
 
 class PostCommentAdmin(admin.ModelAdmin):
     list_display = ["post", "updated_at"]

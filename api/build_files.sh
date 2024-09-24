b0c613e9361ab89ce725540c111573f920d938e4
Matutozi <sobowalegz2@gmail.com>
2024-09-24 12:55:07 +0100

    chore: database migration

--- api/build_files.sh
+++ api/build_files.sh
@@ -3,5 +3,5 @@
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --no-input --clear
 
-python manage.py makemigrations
-python manage.py migrate
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate

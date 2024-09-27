6ca831a1a1f174c7ad00840c02a32d79eb38a63f
Matutozi <sobowalegz2@gmail.com>
2024-09-27 15:54:55 +0100

    fix: changes to schema

--- api/posts/urls.py
+++ api/posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import PostCreateView, PostListView, PostView, PostCommentListCreateView
 
 urlpatterns = [
-    path("create/", PostCreateView.as_view(), name="create-post"),
+    path("create", PostCreateView.as_view(), name="create-post"),
     path("", PostListView.as_view(), name="list-posts"),
      path('<uuid:post_id>/', PostView.as_view(), name='post-detail')
 ]

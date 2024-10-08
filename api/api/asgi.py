75c70d3d0501f4c77e6d63115e79be1304fbc17f
Matutozi <sobowalegz2@gmail.com>
2024-10-09 00:31:55 +0100

    chore: set up of render

--- api/api/asgi.py
+++ api/api/asgi.py
@@ -1,14 +1,14 @@
 import os
 import django
-django.setup()
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-#from channels.security.websocket import AllowedHostsOriginValidator
+# from channels.security.websocket import AllowedHostsOriginValidator
 from message_app.routing import websocket_urlpatterns as message_app_websocket
 from posts.routing import websocket_urlpatterns as posts_websocket
 
@@ -20,10 +20,6 @@ websocket_urlpatterns = message_app_websocket + posts_websocket
 application = ProtocolTypeRouter(
     {
         "http": application,
-        "websocket": (
-            AuthMiddlewareStack(
-                URLRouter(websocket_urlpatterns)
-            )
-        ),
+        "websocket": (AuthMiddlewareStack(URLRouter(websocket_urlpatterns))),
     }
 )

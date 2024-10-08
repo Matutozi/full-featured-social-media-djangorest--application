8e0f2a1bece6df1ae07154428a669c61931ffd86
Matutozi <sobowalegz2@gmail.com>
2024-10-08 19:10:42 +0100

    chore: deployment

--- api/build_files.sh
+++ api/build_files.sh
@@ -3,5 +3,5 @@
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --no-input --clear
 
-python3.9 manage.py makemigrations
+python3.9 manage.py makemigrations --verbosity=3
 python3.9 manage.py migrate

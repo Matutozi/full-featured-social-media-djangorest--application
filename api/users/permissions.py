c1b0ad8c52bd2fffa1d1022cba78c9cd93c270f0
Matutozi <sobowalegz2@gmail.com>
2024-10-24 08:44:51 +0100

    fix: changes to handle specific permission and authentication

--- api/users/permissions.py
+++ api/users/permissions.py
@@ -1,10 +1,17 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, IsAuthenticated
 
 
 class IsAdminorStaff(BasePermission):
     """Class that manages permission for staff anf admin persons"""
 
+    message = "Ban or unban operation fails"
+
     def has_permission(self, request, view):
+        return request.user.is_authenticated and (
+            request.user.is_admin or request.user.is_staff
+        )
+
+    def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and (
             request.user.is_admin or request.user.is_staff
         )
